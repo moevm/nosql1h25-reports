3b1e503eaf4aa44b82cde0c8153ec170215d2ea8
vRedniy-LumbeRt <112636677+vRedniy-LumbeRt@users.noreply.github.com>
2025-04-27 20:33:48 +0300

    space27 recomendation

--- src/diploma_processing/data_types.py
+++ src/diploma_processing/data_types.py
@@ -12,7 +12,7 @@ class Chapter:
     symbols: int
     commonly_used_words: list[str]
     commonly_used_words_amount: list[int]
-    chapters: list['Chapter']
+    chapters: list['Chapter' | int]
 
 
 @dataclass
@@ -24,4 +24,4 @@ class Diploma:
     year: int
     words: int
     load_date: datetime
-    chapters: list[Chapter]
+    chapters: list['Chapter' | int]
