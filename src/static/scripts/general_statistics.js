2d961680e9f4126e30bdd1977304415662808e06
Daniil Lokosov <lokosov.danil@yandex.ru>
2025-05-07 14:35:34 +0300

    idz5 - general statistics arrow close fix

--- src/static/scripts/general_statistics.js
+++ src/static/scripts/general_statistics.js
@@ -3,6 +3,7 @@ $(document).ready(function () {
 
     $('.accordion-header').click(function () {
         $(this).siblings('.accordion-content').find('.accordion-content').hide();
+        $(this).siblings('.accordion-content').find('.arrow').removeClass('rotated');
         $(this).children('.arrow').toggleClass('rotated');
         $(this).siblings('.accordion-content').slideToggle();
     });
